844876d2289a8143ae0924437014d379419443b7
Muonaka Anthony <Antonynans@gmail.com>
2026-04-14 16:39:21 +0100

    changed product display on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
           <div className="max-w-7xl mx-auto px-4 py-12">
             <div className="flex justify-between items-center mb-12">
               <div>
-                <h1 className="text-4xl font-bold text-gray-900 mb-2">
+                <h1 className="md:text-4xl text-2xl font-bold text-gray-900 mb-2">
                   Special Products
                 </h1>
                 <p className="text-gray-600">
@@ -38,7 +38,7 @@ const Home = () => {
 
               <Link
                 to="/shop"
-                className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-8 rounded-lg transition duration-300 flex items-center gap-2"
+                className="bg-blue-600 hover:bg-blue-700 w-fit text-white text-sm font-semibold md:py-3 md:px-8 p-2 rounded-lg transition duration-300 flex items-center gap-2 whitespace-pre"
               >
                 Browse All
                 <span className="text-xl">→</span>
@@ -56,7 +56,8 @@ const Home = () => {
             </div>
 
             {data?.products && data.products.length > 0 ? (
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              <div className="flex gap-6 overflow-x-auto scrollbar-hide snap-x snap-mandatory px-4">
+                {" "}
                 {data.products.map((product) => (
                   <div key={product._id}>
                     <ProductCard p={product} />
