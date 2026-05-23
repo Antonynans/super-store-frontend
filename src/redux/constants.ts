45d2704515c91436808ff4c10441348810675507
antonynans <antonynans@gmail.com>
2026-05-23 07:42:18 +0100

    updated the base endpoint

--- src/redux/constants.ts
+++ src/redux/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://super-store-backend-ws7x.onrender.com";
+export const BASE_URL = "https://super-store-backend-p06k.onrender.com";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
